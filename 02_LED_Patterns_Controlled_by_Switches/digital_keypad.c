4cb70c9c2b4f650dabaa3ce3f5fc85ed9f54bd5a
ShivamAwarde-10 <awardeshivam1@gmail.com>
2026-06-18 00:46:39 +0530

    Update digital_keypad.c

--- 02_LED_Patterns_Controlled_by_Switches/digital_keypad.c
+++ 02_LED_Patterns_Controlled_by_Switches/digital_keypad.c
@@ -1,1 +1,31 @@
+#include <xc.h>                 
+#include "digital_keypad.h"     
+
+void init_digital_keypad(void)
+{
+    TRISC = TRISC | 0x0F;                        // Set RC0-RC3 as input pins for keypad
+}
+
+unsigned char read_digital_keypad(unsigned char trigger)
+{
+    static int once = 1;                               // Static flag to detect single key press (EDGE control)
+
+    if(trigger == LEVEL)                               // If LEVEL mode is selected
+    {
+        return PORTC & INPUT_PINS;                  // Continuously return current keypad status
+    }
+    else if(trigger == EDGE)                           // If EDGE mode is selected
+    {
+        if(((PORTC & INPUT_PINS) != ALL_RELEASED) && (once == 1))
+        {
+            once = 0;                                  // Mark key press detected once
+            return PORTC & INPUT_PINS;              // Return pressed key value
+        }
+        else if((PORTC & INPUT_PINS) == ALL_RELEASED)
+        {
+            once = 1;                                  // Reset flag when all keys are released
+        }
+    }
+    return ALL_RELEASED;                               // Default return when no key is pressed
+}
 
