02f3e27a4e135facb1651483b56fbcdd4a7693d1
ShivamAwarde-10 <awardeshivam1@gmail.com>
2026-06-18 01:21:11 +0530

    Update ssd_display.h

--- 06_Interrupt_Based_Sleep_and_WakeUp_System/ssd_display.h
+++ 06_Interrupt_Based_Sleep_and_WakeUp_System/ssd_display.h
@@ -1,1 +1,13 @@
+#ifndef SSD_DISPLAY_H
+#define SSD_DISPLAY_H
 
+#define ONE     0x21
+#define TWO     0xCB
+#define THREE   0x6B
+#define FOUR    0x2D
+
+void init_ssd_control(void);
+
+void display(unsigned char data[]);
+
+#endif
