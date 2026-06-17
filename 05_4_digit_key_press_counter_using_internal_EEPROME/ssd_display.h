21a0bc413904b44638e623c4f9305e5bc49d3340
ShivamAwarde-10 <awardeshivam1@gmail.com>
2026-06-18 01:08:23 +0530

    Update ssd_display.h

--- 05_4_digit_key_press_counter_using_internal_EEPROME/ssd_display.h
+++ 05_4_digit_key_press_counter_using_internal_EEPROME/ssd_display.h
@@ -1,1 +1,19 @@
+#ifndef SSD_H
+#define SSD_H
 
+#define ZERO    0xE7
+#define ONE     0x21
+#define TWO     0xCB
+#define THREE   0x6B
+#define FOUR    0x2D
+#define FIVE    0x6E
+#define SIX     0xEE
+#define SEVEN   0x23
+#define EIGHT   0xEF
+#define NINE    0x6F
+
+void init_ssd_control(void);
+void display(unsigned char data[]);
+void display_count(unsigned int count);
+
+#endif
