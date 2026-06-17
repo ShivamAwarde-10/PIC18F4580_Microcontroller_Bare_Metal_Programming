36161c72511a1e3d2b1ee07725f58713ea032c1d
ShivamAwarde-10 <awardeshivam1@gmail.com>
2026-06-18 01:19:28 +0530

    Update digital_keypad.c

--- 06_Interrupt_Based_Sleep_and_WakeUp_System/digital_keypad.c
+++ 06_Interrupt_Based_Sleep_and_WakeUp_System/digital_keypad.c
@@ -1,1 +1,27 @@
+#include <xc.h>
+#include "digital_keypad.h"
+ 
+void init_digital_keypad(void)
+{
+    TRISC = TRISC | 0x0F;         // Sets RC0?RC3 bits to 1 (input mode) while leaving RC4?RC7 unchanged
+}
 
+unsigned char read_digital_keypad(unsigned char trigger)
+{
+    static unsigned char once = 1;
+    
+    if(trigger == EDGE)
+    {
+        if(((PORTC & 0x0F) != ALL_RELEASED) && once)
+        {
+            once = 0;
+            
+            return PORTC & 0x0F;
+        }
+        else if((PORTC & 0x0F) == ALL_RELEASED)
+        {
+            once = 1;
+        }
+    }
+    return ALL_RELEASED;
+}
