7f71e75e95886d88d9d1753ff4440477f0d4ea9c
ShivamAwarde-10 <awardeshivam1@gmail.com>
2026-06-18 00:46:10 +0530

    Update main.h

--- 02_LED_Patterns_Controlled_by_Switches/main.h
+++ 02_LED_Patterns_Controlled_by_Switches/main.h
@@ -1,1 +1,11 @@
+#ifndef MAIN_H                 // Prevent multiple inclusion of this header file
+#define MAIN_H
 
+#include <xc.h>                // PIC18F4580 device header file
+
+#define ON   1                 // Define ON state as 1
+#define OFF  0                 // Define OFF state as 0
+
+#define LEDS PORTB             // Define LEDS alias for PORTB register
+
+#endif                         // End of header guard
