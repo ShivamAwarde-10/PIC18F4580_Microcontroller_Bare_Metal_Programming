e1d7c658aa02f74ddc75f389e7376ea6a6ce192f
ShivamAwarde-10 <awardeshivam1@gmail.com>
2026-06-18 01:06:41 +0530

    Update main.c

--- 05_4_digit_key_press_counter_using_internal_EEPROME/main.c
+++ 05_4_digit_key_press_counter_using_internal_EEPROME/main.c
@@ -13,3 +13,79 @@ Inputs:
 2] DKS 1 Long press to reset the count .
 3] DKS 2 Store Input.
 */
+
+#include <xc.h>
+#include "main.h"
+#include "digital_keypad.h"
+#include "ssd_display.h"
+#include "eeprome.h"
+
+void init_config(void) 
+{
+    ADCON1 = 0x0F;
+
+    init_digital_keypad();
+    init_ssd_control();
+}
+
+void main(void) 
+{
+    unsigned char key;                // Stores pressed switch.
+    unsigned long press = 0;          // Counts how long SW1 is pressed.
+    unsigned int count;               // Stores current counter value.
+
+    init_config();
+
+    /* Read saved count from EEPROM */
+    count = read_internal_eeprom(COUNT_ADDR0);
+    count = count | ((unsigned int) read_internal_eeprom(COUNT_ADDR1) << 8);
+
+    while (1) 
+    {
+        key = read_digital_keypad(LEVEL);
+
+        /* SW1 Count Input */
+        if (key == SW1) 
+        {
+            press++;
+
+            if (press > 200) 
+            {
+                press = 200;
+            }
+        } 
+        else 
+        {
+            if (press >= 200) 
+            {
+                count = 0;
+            } 
+            else if (press > 0) 
+            {
+                count++;
+
+                if (count > 9999) 
+                {
+                    count = 0;
+                }
+            }
+
+            press = 0;
+        }
+
+        /* SW2 Store Count */
+        if (read_digital_keypad(EDGE) == SW2) 
+        {
+            unsigned char low_byte;
+            unsigned char high_byte;
+
+            low_byte = count & 0x00FF;             // Lower 8 bits
+            high_byte = (count >> 8) & 0x00FF;     // Upper 8 bits
+
+            write_internal_eeprom(COUNT_ADDR0, low_byte);
+            write_internal_eeprom(COUNT_ADDR1, high_byte);
+        }
+
+        display_count(count);
+    }
+}
