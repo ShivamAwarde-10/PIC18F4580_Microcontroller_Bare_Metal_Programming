d3e2ae08f3e1332e9ace81f0f47190aa03ce95ca
ShivamAwarde-10 <awardeshivam1@gmail.com>
2026-06-18 01:23:36 +0530

    Update main.c

--- 02_LED_Patterns_Controlled_by_Switches/main.c
+++ 02_LED_Patterns_Controlled_by_Switches/main.c
@@ -1,5 +1,6 @@
 /*
-Write a Embedded C program to display the multiple patterns on the LEDs controlled by the switches. For every switch, dedicate a LEDs glow pattern. The pattern should change on key press.
+Write a Embedded C program to display the multiple patterns on the LEDs controlled by the switches. For every switch, 
+dedicate a LEDs glow pattern. The pattern should change on key press.
 
 Output Requirements:
 Upon giving the power supply, all 8 LEDs should be OFF.
@@ -10,12 +11,14 @@ Upon giving the power supply, all 8 LEDs should be OFF.
 4] Press switch-4, LEDs should glow according to the pattern-4*
 
 *pattern-1: The LEDs should glow from Left to Right and Right to left as explained in the assignment-1.
-*pattern-2: The LEDs should glow from left to Right and switch off from left to right, no direction control/ direction change.
+*pattern-2: The LEDs should glow from left to Right and switch off from left to right, no direction control/ 
+            direction change.
 *pattern-3: The LEDs should blink alternately.
-*pattern-4: The LEDs has to blink nibble wise, i.e first 4 LEDs will be ON, next 4 LEDs will be OFF, after first 4 LEDs will be OFF, next 4 LEDs will be ON.
+*pattern-4: The LEDs has to blink nibble wise, i.e first 4 LEDs will be ON, next 4 LEDs will be OFF, 
+            after first 4 LEDs will be OFF, next 4 LEDs will be ON.
 
 Inputs:
-Digital Keypad
+1] Digital Keypad
 */
 
 #include <xc.h>                  
